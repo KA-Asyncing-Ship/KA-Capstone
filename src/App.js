8570db9389052066499de0151746100559c08378
benfindley18 <ben.kenzieacademy@gmail.com>
2020-07-07 12:10:25 -0400

    started add new goal logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import shortid from "shortid";
 import "./App.css";
 import Landing from "./Landing/Landing";
 import TopMenu from "./Menu/TopMenu";
@@ -10,20 +11,74 @@ import GoalModal from "./Goals/GoalModal";
 import Login from "./Login/Login";
 import Details from './Goals/GoalsDetail'
 
-function App() {
-  return (
-    <div className="App">
-      <TopMenu />
-      {/* <Login /> */}
-      {/* <Splash /> */}
-      {/* <Landing /> */}
-      {/* <BudgetSetup /> */}
-      {/* <GoalModal /> */}
-      <Details />
-      {/* <BudgetOverview /> */}
-      <BottomMenu />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    goals: [{
+      id: '',
+      goalName: '',
+      goalAmount: '',
+      depositFrequency: '',
+      depositAmount: '',
+    }],
+    newGoal: [{
+      id: '',
+      goalName: '',
+      goalAmount: '',
+      depositFrequency: '',
+      depositAmount: '',
+    }]
+  }
+
+  handleAddNewGoal = () => {
+    if (this.props.newGoal.goalName !== 0) {
+      let newTaskObject = {
+        id: shortid.generate(),
+        goalName: this.state.newGoal.goalName,
+        goalAmount: this.state.newGoal.goalAmount,
+        depositFrequency: this.state.newGoal.depositFrequency,
+        depositAmount: this.state.newGoal.depositAmount,
+      };
+      this.setState({
+        goals: [...this.state.goals, newTaskObject],
+        newGoal: {
+          id: '',
+          goalName: '',
+          goalAmount: '',
+          depositFrequency: '',
+          depositAmount: '',
+        }
+      });
+    }
+  };
+
+  handleChange = (event) => {
+    event.preventDefault();
+
+    let name = event.target.name;
+    let value = event.target.value;
+
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <TopMenu />
+        {/* <Login /> */}
+        {/* <Splash /> */}
+        {/* <Landing /> */}
+        {/* <BudgetSetup /> */}
+        {/* <GoalModal
+          addNewGoal={this.handleAddNewGoal}
+          change={this.handleChange}
+          goals={this.state.goals}
+          newGoal={this.state.newGoal} /> */}
+        {/* <Details /> */}
+        {/* <BudgetOverview /> */}
+        <BottomMenu />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Goals/GoalModal.js
+++ src/Goals/GoalModal.js
@@ -7,36 +7,42 @@ import {
 
 Modal.setAppElement('#root')
 
-function NewGoal() {
+function NewGoal(props) {
     const [modalIsOpen, setModalIsOpen] = useState(false)
     return (
         <>
             <img className='plus' onClick={() => setModalIsOpen(true)} src="https://img.icons8.com/ios/50/000000/add.png" />
-            <div className='emptyStaticScreen'>
+            {/* <div className='emptyStaticScreen'>
                 <p>You have not created any savings goals yet, click the plus button to get started on your first goal!</p>
                 <img src="https://img.icons8.com/ios/100/000000/sad-cloud.png" />
+            </div> */}
+
+            <div className='addedGoals'>
+                <p>fuck</p>
+                {props.goals.map((goalObject) => (
+                    <p value={props.goals.goalName}></p>
+                ))}
             </div>
 
-            <div className='footer'>
 
-            </div>
+
 
             <Modal className='modal' isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
                 <div className='formDiv'>
-                    <form className='goalForm' id='goalForm'>
+                    <form className='goalForm' id='goalForm' onSubmit={props.addNewGoal}>
                         <div className='newGoalName'>
                             <label className='label' htmlFor='goalName'> Goal Name: </label> <br />
-                            <input type='text' name='goalName' className='goalName input' />
+                            <input onChange={props.change} value={props.newGoal.goalName} type='text' name='goalName' className='goalName input' />
                         </div>
 
                         <div className='newGoalAmount'>
                             <label className='label' htmlFor='goalAmount'> Goal Amount: </label> <br />
-                            <input type='number' name='goalAmount' className='goalAmount input' min='0.00' step='0.01' />
+                            <input onChange={props.change} value={props.newGoal.goalAmount} type='number' name='goalAmount' className='goalAmount input' min='0.00' step='0.01' />
                         </div>
 
                         <div className='NewDepositFrequency'>
                             <label className='label' htmlFor='depositFrequency'> Deposit Frequency: </label> <br />
-                            <select className='depositFrequency input' name='depositFrequency' form='goalForm'>
+                            <select onChange={props.change} value={props.newGoal.depositFrequency} className='depositFrequency' name='depositFrequency' form='goalForm'>
                                 <option value='weekly'> Weekly </option>
                                 <option value='bi-weekly'> Bi-Weekly </option>
                                 <option value='monthly'> Monthly </option>
@@ -45,10 +51,8 @@ function NewGoal() {
 
                         <div className='NewDepositAmount'>
                             <label className='label' htmlFor='depositAmount'> Deposit Amount: </label> <br />
-                            <input type='number' name='goalAmount' className='depositAmount input' min='0.00' step='0.01' />
+                            <input onChange={props.change} value={props.newGoal.depositAmount} type='number' name='depositAmount' className='depositAmount input' min='0.00' step='0.01' />
                         </div>
-
-                        {/* <input type="submit" value="Save Goal" className='saveGoal' /> */}
                     </form>
 
                     <div className='submitBtn'>
